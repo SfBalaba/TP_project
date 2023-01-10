58fec250af9ecf6c047ec8b2db96543596369567
BalabaSofia <102922461+SfBalaba@users.noreply.github.com>
2023-01-10 12:43:33 +0500

    add new pages: demand, geography

--- analytics/main/urls.py
+++ analytics/main/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='home'),
+
+    path("home/", views.index, name='home'),
 path("demand/", views.demand, name='demand'),
 path("geography/", views.geography, name='geography'),
 path("skills/", views.skills, name='skills'),
 path("latest-vacancies/", views.latestVacancies, name='latest-vacancies'),
+
+    path('index/', views.index1)
+
 
 ]
