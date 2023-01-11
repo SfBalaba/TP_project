20ab4d7d881be67d086b2d076859a2e29a92f408
BalabaSofia <102922461+SfBalaba@users.noreply.github.com>
2023-01-11 17:53:36 +0500

    add skills statistics

--- analytics/main/views.py
+++ analytics/main/views.py
@@ -1,4 +1,4 @@
-from msilib.schema import Environment
+
 
 import openpyxl
 from django.shortcuts import render
@@ -8,6 +8,17 @@ from xlsx2html import xlsx2html
 
 def index(request):
     return render(request, 'main/home.html')
+
+def insert_table(file_name, sheet_name):
+
+    wb = openpyxl.load_workbook('C:/Users/anony/app/analytics/main/static/img/report.xlsx')
+    worksheet = wb["Статистика по годам"]
+    excel_data = list()
+    for row in worksheet.iter_rows():
+        row_data = list()
+        for cell in row:
+            row_data.append(str(cell.value))
+        excel_data.append(row_data)
 
 def demand(request):
     vac_name = "web-разработчик"
@@ -38,7 +49,17 @@ def geography(request):
     return render(request, 'main/geography.html', {'name_vacancy': vac_name, "excel_data": excel_data})
 
 def skills(request):
-    return render(request, 'main/skills.html')
+    vac_name = "web-разработчик"
+    wb = openpyxl.load_workbook('C:/Users/anony/app/analytics/main/static/img/skills_stat.xlsx')
+    worksheet = wb["Навыки"]
+
+    excel_data = list()
+    for row in worksheet.iter_rows():
+        row_data = list()
+        for cell in row:
+            row_data.append(str(cell.value))
+        excel_data.append(row_data)
+    return render(request, 'main/skills.html', {'name_vacancy': vac_name, "excel_data": excel_data})
 
 def latestVacancies(request):
     return render(request, 'main/latest-vacancies.html')
