550bac833d67037b7b8866e688879a77e205a3d0
BalabaSofia <102922461+SfBalaba@users.noreply.github.com>
2023-01-19 01:57:55 +0500

    update latest_vacancies page

--- analytics/main/services.py
+++ analytics/main/services.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 from typing import List, Dict
 import pandas as pd
 import requests
@@ -23,7 +24,11 @@ def data_from_xlsx(path):
     for row in range(ws.nrows):
         row_data = list()
         for col in range(ws.ncols):
-            row_data.append(ws.cell(row, col).value)
+            cell_value = ws.cell(row, col).value
+
+            value = int(cell_value)\
+                if ( len(str(cell_value))!=0 and str(cell_value)[-1] == '0') else cell_value
+            row_data.append(value)
         excel_data.append(row_data)
     return excel_data, vac_name
 
@@ -32,81 +37,112 @@ url = "https://api.hh.ru/vacancies"
 def execute_vacancies(vacancies: List[Dict[str, str]] or List[Dict[Dict[str, str], str]]) -> (List[List[str]]):
     """ формирует дату для создания итогового датафрейма
     оставляет только нужные поля: название, город, вилка оклада, дата публиукации вакансии
-
     Args:
         vacancies: список словарей выгруженных api содержащие информацию по вакасии
-
     Returns: список вакансий c зарплатами
-
     """
     return [
         [
             vacancy['id'],
             vacancy["name"],
-            vacancy["employer"]["name"],
+            vacancy["area"]["name"],
             vacancy["salary"]["from"],
             vacancy["salary"]["to"],
             vacancy["salary"]["currency"],
-            vacancy["area"]["name"],
             vacancy["published_at"],
         ]
         for vacancy in vacancies
         if vacancy["salary"]
-        ]
+    ]
 
 def get_vacancies_items(params: dict) -> (List[Dict[str, str]] or List[Dict[Dict[str, str], str]]):
     """ поулчает json данные о вакансии по заданынм параметрам
-
     Args:
         params: параметры для запроса
-
     Returns: данные по вакансиям
-
     """
     return requests.get(url, params).json()["items"]
 
+
 def get_latest_vac():
+
     pd.set_option('expand_frame_repr', False)
-    params = dict(
-        specialization=1,
-        date_from="2022-12-26T00:00:00",
-        date_to="2022-12-26T12:00:00",
-        per_page=30,
-        page=1,
-    )
-    res = requests.get(url, params).json()['items']
-    res = execute_vacancies(res)
-    for i, el in enumerate(res):
-        new_get = requests.get(f"https://api.hh.ru/vacancies/{el[0]}").json()
-        el.pop(0)
 
-        el.append(new_get['description'])
-        if (len(new_get['key_skills']) != 0):
-            el.append(", ".join([x['name'] for x in new_get['key_skills']]))
+    pages_count = 20
+    params1 = [
+        dict(
+            specialization=1,
+            date_from="2022-12-15T00:00:00",
+            date_to="2022-12-15T12:00:00",
+            per_page=100,
+            page=i,
+        )
+        for i in range(pages_count)
+    ]
+
+    params2 = [
+        dict(
+            specialization=1,
+            date_from="2022-12-15T12:00:00",
+            date_to="2022-12-16T00:00:00",
+            per_page=100,
+            page=i,
+        )
+        for i in range(pages_count)
+    ]
+
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        result = list(executor.map(get_vacancies_items, params1 + params2))
+        response = list(executor.map(execute_vacancies, result))
+        result = pd.concat(
+            [
+                pd.DataFrame(
+                    el,
+                    columns=[
+                        'id',
+                        "name",
+                        "area_name",
+                        "salary_from",
+                        "salary_to",
+                        "salary_currency",
+                        "published_at"
+                    ]
+                )
+                for el in response
+            ]
+        )
+    vacancy_name = ['web-develop', 'веб-разработчик', 'web-разработчик', 'web programmer', 'web программист','битрикс',
+                    'веб программист', 'битрикс разработчик', 'bitrix разработчик', 'drupal разработчик',
+                    'cms разработчик',
+                    'wordpress разработчик', 'wp разработчик', 'joomla разработчик', 'drupal developer',
+                    'cms developer',
+                    'wordpress developer', 'wp developer', 'joomla developer']
+    flag = True
+    for vac in vacancy_name:
+        if (flag):
+            df2 = result[result['name'].str.contains(vac, flags=re.IGNORECASE,
+                                                     regex=True)]
         else:
-            res.pop(i)
-    res = [x for x in res if len(x) == 9]
-    df = pd.DataFrame(
-        res,
-        columns=[
-            "name",
-            "company",
-            "salary_from",
-            "salary_to",
-            "salary_currency",
-            "area_name",
-            "published_at",
-            "discription",
-            "skills",
-        ],
-    )
-    df["published_at"] = df["published_at"].apply(lambda x: x[:10])
-    df['salary'] = df['salary_from'] + df["salary_to"] / 2
-    df.loc[df["salary_to"].isna(), ['salary']] = df["salary_from"]
-    df = df.loc[df['salary_currency'] == 'RUR']
+            new = result[result['name'].str.contains(vac, flags=re.IGNORECASE,
+                                                     regex=True)]
+            df2 = pd.concat([df2, new], axis=0, ignore_index=True)
+        flag = False
+    df2 = df2.head(10)
+    for i in range(df2.shape[0]):
+        new_get = requests.get(f"https://api.hh.ru/vacancies/{df2.loc[[i], ['id']].values[0][0]}").json()
+        df2.loc[[i], ['company']] = new_get["employer"]["name"]
+        df2.loc[[i], ['description']] = new_get['description']
+        if (len(new_get['key_skills']) != 0):
+            df2.loc[[i], ['key_skills']] = ", ".join([x['name'] for x in new_get['key_skills']])
+        else:
+            df2.loc[[i], ['key_skills']] = ""
 
-    df = df.drop(["salary_from", "salary_to"], axis=1)
-    df = df.head(10)
-    json_records = df.reset_index().to_json(orient='records')
+    df2["published_at"] = df2["published_at"].apply(lambda x: x[:10])
+    df2['salary'] = df2[['salary_from', 'salary_to']].mean(axis=1)
+    df2 = df2.dropna(subset=['salary_from', 'salary_to'], how='all').reset_index(drop=True)
+    df2 = df2.loc[df2['salary_currency'] == 'RUR']
+    df2 = df2.drop(["salary_from", "salary_to"], axis=1)
+    df2 = df2.drop(['id'], axis=1)
+    json_records = df2.reset_index().to_json(orient='records')
     data = json.loads(json_records)
     return data
